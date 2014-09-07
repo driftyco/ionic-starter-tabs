2376dffd1d95699d4431858333837b27faaec101
steve mao <steve.mao@healthinteract.com.au>
2014-09-08 09:57:25 +1000

    Use camelCase for factory

--- js/controllers.js
+++ js/controllers.js
@@ -3,12 +3,12 @@ angular.module('starter.controllers', [])
 .controller('DashCtrl', function($scope) {
 })
 
-.controller('FriendsCtrl', function($scope, Friends) {
-  $scope.friends = Friends.all();
+.controller('FriendsCtrl', function($scope, friends) {
+  $scope.friends = friends.all();
 })
 
-.controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
-  $scope.friend = Friends.get($stateParams.friendId);
+.controller('FriendDetailCtrl', function($scope, $stateParams, friends) {
+  $scope.friend = friends.get($stateParams.friendId);
 })
 
 .controller('AccountCtrl', function($scope) {
